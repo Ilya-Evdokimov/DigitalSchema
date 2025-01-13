4401a5e192411aad72b9eb003c88c262bda223d5
Ilya_Evdokimov <blinix03yt@gmail.com>
2025-01-13 20:26:51 +0500

    13.01.2025

---
+++ DigitalSchema/Chips/Chip3And.xaml.cs
@@ -0,0 +1,9 @@
+namespace DigitalSchema.Chips;
+
+public partial class Chip3And : ContentView
+{
+	public Chip3And()
+	{
+		InitializeComponent();
+	}
+}
---
+++ DigitalSchema/Chips/ChipAndOrKE.xaml.cs
@@ -0,0 +1,9 @@
+namespace DigitalSchema.Chips;
+
+public partial class ChipAndOrKE : ContentView
+{
+	public ChipAndOrKE()
+	{
+		InitializeComponent();
+	}
+}
