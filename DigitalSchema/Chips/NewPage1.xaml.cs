aaea7e9df5bf0b6cddf5f571be3c3527b3b0e28b
Ilya_Evdokimov <blinix03yt@gmail.com>
2025-02-02 20:02:59 +0500

    02.02.2025-20.02

--- DigitalSchema/Chips/NewPage1.xaml.cs
+++ DigitalSchema/Chips/NewPage1.xaml.cs
@@ -1,17 +1,111 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 namespace DigitalSchema.Chips
 {
-    public partial class NewPage1 : ContentView
+    public partial class NewPage1 : ContentView, INotifyPropertyChanged
     {
-        bool isEllipsePressed = false;
-        public NewPage1()
+        private Color _InputValues_01 = Colors.Transparent;
+        private Color _InputValues_02 = Colors.Red;
+        private Color _InputValues_03 = Colors.Red;
+        private Color _InputValues_04 = Colors.Red;
+        private Color _OutputValues;
+
+        public Color InputValues_01
         {
-            InitializeComponent();
+            get => _InputValues_01;
+            set
+            {
+                _InputValues_01 = value;
+                OnPropertyChanged();
+                ProcessedValues_NewPage1();
+            }
+        }
+        public Color InputValues_02
+        {
+            get => _InputValues_02;
+            set
+            {
+                _InputValues_02 = value;
+                OnPropertyChanged();
+                ProcessedValues_NewPage1();
+            }
+        }
+        public Color InputValues_03
+        {
+            get => _InputValues_03;
+            set
+            {
+                _InputValues_03 = value;
+                OnPropertyChanged();
+                ProcessedValues_NewPage1();
+            }
+        }
+        public Color InputValues_04
+        {
+            get => _InputValues_04;
+            set
+            {
+                _InputValues_04 = value;
+                OnPropertyChanged();
+                ProcessedValues_NewPage1();
+            }
+        }
+        public Color OutputValues
+        {
+            get => _OutputValues;
+            set
+            {
+                _OutputValues = value;
+                OnPropertyChanged();
+            }
+        }
+        public void ProcessedValues_NewPage1()
+        {
+            int OutputValuesFloat = DigitalConverter(InputValues_01) * DigitalConverter(InputValues_02) * DigitalConverter(InputValues_03) * DigitalConverter(InputValues_04);
+            OutputValues = OutputValuesFloat == 1 ? Colors.Transparent : Colors.Red;
+        }
+
+        public int DigitalConverter(Color InputValues)
+        {
+            return InputValues == Colors.Transparent ? 0 : 1;
         }
         public void OnEllipseTapped(object sender, EventArgs e)
         {
-            isEllipsePressed = !isEllipsePressed;
-            In_1.Fill = isEllipsePressed ? Colors.Blue : Colors.Red;
-        }
+            if (sender is View view && view.GestureRecognizers.FirstOrDefault() is TapGestureRecognizer tapGesture)
+            {
+                string propertyName = tapGesture.CommandParameter as string;
 
+                switch (propertyName)
+                {
+                    case "s_InputValues_01":
+                        InputValues_01 = InputValues_01 == Colors.Transparent ? Colors.Red : Colors.Transparent;
+                        break;
+                    case "s_InputValues_02":
+                        InputValues_02 = InputValues_02 == Colors.Transparent ? Colors.Red : Colors.Transparent;
+                        break;
+                    case "s_InputValues_03":
+                        InputValues_03 = InputValues_03 == Colors.Transparent ? Colors.Red : Colors.Transparent;
+                        break;
+                    case "s_InputValues_04":
+                        InputValues_04 = InputValues_04 == Colors.Transparent ? Colors.Red : Colors.Transparent;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        public NewPage1()
+        {
+            InitializeComponent();
+            BindingContext = this;
+            ProcessedValues_NewPage1();
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- DigitalSchema/MainPage.xaml.cs
+++ DigitalSchema/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using DigitalSchema.Chips;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 namespace DigitalSchema
